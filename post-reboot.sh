189b18cb3c7f31c3c131db49f98cdb1adfa19cdf
arielbogachov <132680862+arielbogachov@users.noreply.github.com>
2024-05-28 15:10:46 +0300

    Update post-reboot.sh

--- post-reboot.sh
+++ post-reboot.sh
@@ -4,8 +4,8 @@ POSTSEC_REBOOT_SCRIPT="/var/tmp/removetemp.sh"
 LOG_FILE="/var/tmp/post_reboot.log"
 STATE_FILE="/var/tmp/reboot.txt"
 NUM_FILE="/var/tmp/hostname_num.txt"
-orcaghost="orca-ghost"
-password="Orca123"
+user="<username>"
+password="<password>"
 
 sleep 60 
 
@@ -29,19 +29,19 @@ num=$(cat "$NUM_FILE")
 echo "Hostname number is: $num"
 
 # Check if orca-ghost user is logged in and log out
-if who | grep -q "$orcaghost"; then
-    echo "User $orcaghost is currently logged in. Logging out user..."
-    sudo pkill -u "$orcaghost" || { echo "Failed to kill processes owned by $orcaghost"; exit 1; }
+if who | grep -q "$user"; then
+    echo "User $user is currently logged in. Logging out user..."
+    sudo pkill -u "$user" || { echo "Failed to kill processes owned by $user"; exit 1; }
 fi
 # Rename orca-ghost
 echo "Renaming $orcaghost to $num..."
-echo "$password" | sudo usermod -l "$num" "$orcaghost" || { echo "Failed to rename user"; exit 1; }
+echo "$password" | sudo usermod -l "$num" "$user" || { echo "Failed to rename user"; exit 1; }
 echo "$password" | sudo usermod -d "/home/$num" -m "$num" || { echo "Failed to move home directory"; exit 1; }
-echo "$password" | sudo ln -s  "/home/$num" "/home/$orcaghost" || { echo "Failed to create symlink"; exit 1; }
+echo "$password" | sudo ln -s  "/home/$num" "/home/$user" || { echo "Failed to create symlink"; exit 1; }
 
 # Update the python environment or other configurations as needed
 echo "Updating python environment..."
-echo "$password" | sudo -S sed -i "s/$orcaghost/$num/g" "/home/$num/.pyenv/versions/3.7.4/bin/pip" || { echo "Failed to update python version"; exit 1; }
+echo "$password" | sudo -S sed -i "s/$user/$num/g" "/home/$num/.pyenv/versions/3.7.4/bin/pip" || { echo "Failed to update python version"; exit 1; }
 
 
 # Additional configuration
@@ -50,7 +50,7 @@ echo "$password" | sudo -S bash -c "echo -e '[daemon]\nAutomaticLoginEnable=true
 
 #removes the post script from crontab
 (crontab -l -u temp 2>/dev/null | grep -v "@reboot $POST_REBOOT_SCRIPT") | sudo crontab -u temp -
-echo "Updated crontab for user $temp:"
+echo "Updated crontab for user temp:"
 sudo crontab -l -u temp
 
 #make the another reboot script
